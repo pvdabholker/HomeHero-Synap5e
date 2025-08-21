0cbd5488de9ac429788f78b4eee52f3b94dbf5d2
Sidnaik04 <naiksidhant04@gmail.com>
2025-08-21 20:18:45 +0530

    Setup database

---
+++ frontend/.expo/types/router.d.ts
@@ -0,0 +1,14 @@
+/* eslint-disable */
+import * as Router from 'expo-router';
+
+export * from 'expo-router';
+
+declare module 'expo-router' {
+  export namespace ExpoRouter {
+    export interface __routes<T extends string | object = string> {
+      hrefInputParams: { pathname: Router.RelativePathString, params?: Router.UnknownInputParams } | { pathname: Router.ExternalPathString, params?: Router.UnknownInputParams } | { pathname: `/`; params?: Router.UnknownInputParams; } | { pathname: `/_sitemap`; params?: Router.UnknownInputParams; } | { pathname: `/auth/customer/customerLogin`; params?: Router.UnknownInputParams; } | { pathname: `/auth/serviceProvider/serviceProviderLogin`; params?: Router.UnknownInputParams; };
+      hrefOutputParams: { pathname: Router.RelativePathString, params?: Router.UnknownOutputParams } | { pathname: Router.ExternalPathString, params?: Router.UnknownOutputParams } | { pathname: `/`; params?: Router.UnknownOutputParams; } | { pathname: `/_sitemap`; params?: Router.UnknownOutputParams; } | { pathname: `/auth/customer/customerLogin`; params?: Router.UnknownOutputParams; } | { pathname: `/auth/serviceProvider/serviceProviderLogin`; params?: Router.UnknownOutputParams; };
+      href: Router.RelativePathString | Router.ExternalPathString | `/${`?${string}` | `#${string}` | ''}` | `/_sitemap${`?${string}` | `#${string}` | ''}` | `/auth/customer/customerLogin${`?${string}` | `#${string}` | ''}` | `/auth/serviceProvider/serviceProviderLogin${`?${string}` | `#${string}` | ''}` | { pathname: Router.RelativePathString, params?: Router.UnknownInputParams } | { pathname: Router.ExternalPathString, params?: Router.UnknownInputParams } | { pathname: `/`; params?: Router.UnknownInputParams; } | { pathname: `/_sitemap`; params?: Router.UnknownInputParams; } | { pathname: `/auth/customer/customerLogin`; params?: Router.UnknownInputParams; } | { pathname: `/auth/serviceProvider/serviceProviderLogin`; params?: Router.UnknownInputParams; };
+    }
+  }
+}
