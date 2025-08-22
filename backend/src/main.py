b59db1e3d090740eb80044ad296d72f3c18dd795
Sidnaik04 <naiksidhant04@gmail.com>
2025-08-22 07:29:15 +0530

    Add create user and update location

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
 from .database import engine
 from . import models
-from .routes import customers, providers, admin
+from .routes import customers, providers, admin, user
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+app.include_router(user.router)
 
 app.include_router(customers.router, prefix="/api", tags=["customer"])
 app.include_router(providers.router, prefix="/api", tags=["provider"])
--- backend/src/models.py
+++ backend/src/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, String, Float, Boolean, DateTime, ForeignKey, ARRAY
+from sqlalchemy import Column, String, Float, Boolean, DateTime, ForeignKey, ARRAY, func
 from sqlalchemy.orm import relationship
 from sqlalchemy.dialects.postgresql import UUID
 import uuid
@@ -8,13 +8,23 @@ from .database import Base
 class User(Base):
     __tablename__ = "users"
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    name = Column(String, index=True)
-    email = Column(String, unique=True, index=True)
-    phone = Column(String, unique=True, index=True)
-    user_type = Column(String)
-    location = Column(String, nullable=True)
-    created_at = Column(DateTime)
-    provider = relationship("Provider", back_populates="user", uselist=False)
+    name = Column(String, index=True, nullable=False)
+    email = Column(String, unique=True, index=True, nullable=False)
+    phone = Column(String, unique=True, index=True, nullable=False)
+    # user roles
+    user_type = Column(String, nullable=False, default="customer")
+
+    # Locations -> Tailored for Goa, India
+    address_line = Column(String, nullable=True)
+    city = Column(String, nullable=True)
+    pincode = Column(String, index=True, nullable=True)
+
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
+
+    provider = relationship(
+        "Provider", back_populates="user", uselist=False, cascade="all, delete-orphan"
+    )
 
 
 class Provider(Base):
